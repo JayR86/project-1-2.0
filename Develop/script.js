cda7b9aba76c396766a8b9e9106ffa1725d802f0
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-01-30 21:23:30 -0800

    started javascript code for weather api

---
+++ Develop/script.js
@@ -0,0 +1,24 @@
+let weather = {
+    apiKey: "debedf3ec770a6134bafe0d07ed54e45",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+
+    }
+}
